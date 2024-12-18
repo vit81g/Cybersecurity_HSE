59662349dd775fd7220c55c07b6228680ea841bd
Vitaliy S. Novikov <65821524+vit81g@users.noreply.github.com>
2024-12-18 15:37:15 +0300

    Vebinar

---
+++ HomeWorks/Python_basics/test/test.py
@@ -0,0 +1,20 @@
+
+num1 = 2
+
+num2 = float(2)
+
+#int num3 = 2
+
+#var num4 = 2
+
+print(num1)
+print(num2)
+#print(num3)
+#print(num4)
+
+
+a = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge']
+print(a[-6])
+print(a[-5:-3])
+print(a[4::-2])
+
